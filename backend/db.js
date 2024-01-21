6dcc75c95b36f29e65a6c534e19afc155877446b
yash-not-bot22 <yashbaghel.aai@gmail.com>
2024-01-21 16:06:05 +0530

    y

--- backend/db.js
+++ backend/db.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   const connectToMongoose = async () => {
-    await mongoose.connect(process.env.REACT_APP_MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Connected to myDB");
   }
   connectToMongoose().catch((err) => console.error(err));
