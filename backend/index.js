96395f14b3c3073d5a6366c9d94171b2110349a5
yash-not-bot22 <yashbaghel.aai@gmail.com>
2024-01-21 12:33:46 +0530

    f

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,14 @@ const app = express()
 const port = 5000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin:"https://notes-zeta-six.vercel.app"
+}))
 
 //available routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
-app.get("/",(req,res)=>{
-res.send("server running")
-});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
