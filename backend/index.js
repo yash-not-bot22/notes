2713eca827dd660705d41ac80743dc364b56eddf
yash-not-bot22 <yashbaghel.aai@gmail.com>
2024-01-21 12:00:49 +0530

    g

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,9 @@ app.use(cors())
 //available routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
-
+app.get("/",(req,res)=>{
+res.send("server running")
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
